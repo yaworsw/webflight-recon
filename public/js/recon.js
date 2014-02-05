040f5cc39e8599033bd4846f0b89964555f3f1f5
William Yaworsky <wj.px01@gmail.com>
2014-02-05 08:44:55 -0500

    Added some basic notification popups

--- public/js/recon.js
+++ public/js/recon.js
@@ -4,21 +4,22 @@
       cameraUrl     = '/camera',
       canvas        = undefined;
 
-  var successHandler = function(result) {
-
-  };
-
-  var errorHandler = function(result) {
-
-  };
-
   var Recon = function(cockpit) {
     console.log("Loading Recon plugin.");
 
-    this.cockpit = cockpit;
     canvas = $('canvas')[0];
+
+    this.cockpit = cockpit;
+
+    this.initialize();
     this.listen();
   };
+
+  Recon.prototype.initialize = function() {
+    $('body').append('<div id="recon-container"><div class="notifications"></div></div>');
+    this.container     = $('#recon-container');
+    this.notifications = this.container.find('.notifications');
+  }
 
   Recon.prototype.listen = function() {
     var self = this;
@@ -29,14 +30,39 @@
 
   Recon.prototype.keyDown = function(e) {
     if (e.keyCode == cameraKeyCode) {
+      var self  = this;
+      var image = canvas.toDataURL();
       $.ajax({
         type: 'POST',
         url:  cameraUrl,
-        data: { image: canvas.toDataURL() },
-        success: successHandler,
-        error: errorHandler
+        data: { image: image },
+        success: function() {
+          self.notify({
+            thumbnail: image,
+            text:      'Image successfully saved.'
+          });
+        },
+        error: function() {
+          self.notify({
+            class: 'error',
+            text:  'There was an error while trying to save your image.'
+          });
+        }
       })
     }
+  };
+
+  Recon.prototype.notify = function(spec) {
+    var notification = $('<div class="notification ' + (spec.class || '') + '" style="display:none"></div>');
+    spec.thumbnail && notification.append('<img class="thumbnail" src="' + spec.thumbnail + '"/>');
+    spec.text      && notification.append('<span class="text">' + spec.text + '</span>');
+    this.notifications.append(notification);
+    notification.slideDown();
+    setTimeout(function() {
+      notification.slideUp('normal', function() {
+        $(this).remove();
+      });
+    }, 1500);
   };
 
   window.Cockpit.plugins.push(Recon);
